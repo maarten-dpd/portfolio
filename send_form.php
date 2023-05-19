b94a0e4d5026230342a376a4901fbe946fa19afc
maarten-dpd <66560389+maarten-dpd@users.noreply.github.com>
2023-05-19 12:55:04 +0200

    commitje

---
+++ send_form.php
@@ -0,0 +1,30 @@
+<?php
+// formulier data ophalen
+$name = $_POST['naam'];
+$email = $_POST['email'];
+$skills = $_POST['selecteerSkills'];
+$comments = $_POST['commentaarEnFeedback'];
+
+// email aanmaken
+$to = 'maartendp@gmail.com';
+$subject = 'Contact Form portfolio';
+$message = "Name: $name\n\n";
+$message .= "Email: $email\n\n";
+$message .= "Skills: $skills\n\n";
+$message .= "Comments: $comments\n\n";
+$headers = "From: $email\r\n" .
+           "Reply-To: $email\r\n" .
+           "X-Mailer: PHP/" . phpversion();
+
+// email sturen
+$mailSent = mail($to, $subject, $message, $headers);
+
+// controle
+if ($mailSent) {
+  // Email verstuurd
+  echo 'success';
+} else {
+  // email niet verstuurd
+  echo 'error';
+}
+?>
